275ba9f3d8516d4fcf252d8050cef484f008ca84
Artdin72 <artdin2013@gmail.com>
2024-11-20 17:16:42 +0300

    DZ_var

---
+++ Modul_1_var.py
@@ -0,0 +1,5 @@
+total_homework = 12
+volume_hours = 1.5
+name_course = 'Python'
+time_homework = volume_hours / total_homework
+print('Курс:' , name_course,',', 'всего задач:', total_homework, ',', 'затрачено часов: ', volume_hours, ',', 'среднее время выполнения ', time_homework, 'часа.')
