03da9bce8eef7e1740af07d528f0816a7dd7f16a
Artdin72 <artdin2013@gmail.com>
2024-11-26 16:16:02 +0300

    DZ_Tuple

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+#2
+immutable_var = 5, 'cat', False, 0.99
+print(immutable_var)
+#3 Изменение значений переменных
+#immutable_var [1] = 'dog' #№ изменить это не возможно, т.к. cat и dog ивечные природные враги ;)
+#4
+immutable_list = [1000, 7, 'The beatles']
+print(immutable_list)
+immutable_list[2] = 'The rolling stones' #ну, тут дело вкуса...
+print(immutable_list)
