8d88acd45df6fd339d176199c7883342bc436fc7
Artdin72 <artdin2013@gmail.com>
2024-12-01 12:41:43 +0300

    DZ_var_beautiful

--- Modul_1_var.py
+++ Modul_1_var.py
@@ -2,4 +2,7 @@ total_homework = 12
 volume_hours = 1.5
 name_course = 'Python'
 time_homework = volume_hours / total_homework
-print('Курс:' , name_course,',', 'всего задач:', total_homework, ',', 'затрачено часов: ', volume_hours, ',', 'среднее время выполнения ', time_homework, 'часа.')
+print('Курс:' , name_course,',',
+      'всего задач:', total_homework, ',',
+      'затрачено часов: ', volume_hours, ',',
+      'среднее время выполнения ', time_homework, 'часа.')
