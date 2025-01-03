0ffb499a9755e631b61691d87d682174910c76f4
Artdin72 <artdin2013@gmail.com>
2025-01-03 20:10:21 +0300

    DZ_M5_spec_method

--- Module_5_atrib&metod.py
+++ Module_5_atrib&metod.py
@@ -3,7 +3,6 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
-
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
             print('"Такого этажа не существует"')
@@ -11,8 +10,22 @@ class House:
             for i in range(1,new_floor+1):
                 print(i)
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
 h1 = House('Мечта', 15)
 h2 = House('Простоквашино', 3)
 
 h1.go_to(6)
 h2.go_to(4)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
