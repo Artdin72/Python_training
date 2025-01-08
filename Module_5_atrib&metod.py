e107d1c82c4ddae8de3bc2676576ae3d851cfd11
Artdin72 <artdin2013@gmail.com>
2025-01-09 00:03:42 +0300

    DZ_M5_upload_oper

--- Module_5_atrib&metod.py
+++ Module_5_atrib&metod.py
@@ -16,6 +16,37 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
 
+    def __eq__(self, other):
+        if not isinstance((self or other), House):
+            print('Сравнение невозможно.Объекты разных классов')
+        return self.number_of_floors == other.number_of_floors
+
+    def __add__(self, other):
+        if not isinstance(other, int):
+            print('Увеличение этажей невозможно.Число должно быть целым')
+        return House(self.name, self.number_of_floors + other)
+
+    def __iadd__(self, other):
+        return House(self.name, self.number_of_floors + other)
+
+    def __radd__(self, other):
+        return House(self.name, self.number_of_floors + other)
+
+    def __gt__(self, other):
+        return  self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return  self.number_of_floors >= other.number_of_floors
+
+    def __lt__(self, other):
+        return  self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return  self.number_of_floors <= other.number_of_floors
+
+    def __ne__(self, other):
+        return  self.number_of_floors != other.number_of_floors
+
 h1 = House('Мечта', 15)
 h2 = House('Простоквашино', 3)
 
@@ -29,3 +60,25 @@ print(h2)
 # __len__
 print(len(h1))
 print(len(h2))
+
+print(h1 == h2) # __eq__
+
+h2 = h2 + 12 # __add__
+print(h2)
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+
+print(h1 >= h2)  # __ge__
+
+print(h1 < h2)  # __lt__
+
+print(h1 <= h2)  # __le__
+
+print(h1 != h2)  # __ne__
